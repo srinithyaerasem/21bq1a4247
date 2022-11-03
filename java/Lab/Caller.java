5b375d43d09d8f1db88cb9de02bcfa6bbcb77e60
srinithyaerasem <21bq1a4247@vvit.net>
2022-11-03 21:33:24 +0530

    multithreads

---
+++ java/Lab/Caller.java
@@ -0,0 +1,16 @@
+package p2;
+public class Caller implements Runnable {
+	String msg;
+	Callme target;
+	Thread t;
+	public Caller(Callme targ,String s) {
+		target=targ;
+		msg=s;
+		t=new Thread(this);
+	}
+	public void run() {
+		target.call(msg);
+	}
+	
+
+}
